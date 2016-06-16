2fb82826b60135327d0261cc098daf3579f3a12b
nvisanji <nvisanj@nadeem-v.local>
2016-06-16 11:30:24 -0700

    Initial Commit

---
+++ Can't Decide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Can't Decide
+//
+//  Created by Nadeem Visanji on 6/16/16.
+//  Copyright © 2016 Nadeem Visanji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Can't DecideTests/Can_t_DecideTests.swift
@@ -0,0 +1,36 @@
+//
+//  Can_t_DecideTests.swift
+//  Can't DecideTests
+//
+//  Created by Nadeem Visanji on 6/16/16.
+//  Copyright © 2016 Nadeem Visanji. All rights reserved.
+//
+
+import XCTest
+@testable import Can_t_Decide
+
+class Can_t_DecideTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
